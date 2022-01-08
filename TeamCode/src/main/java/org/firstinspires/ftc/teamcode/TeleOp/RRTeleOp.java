0e2ffbe3975e2a7fc79a309235af5a51286b39f1
JonCoombs <22coombs2@lexingtonma.org>
2022-01-08 14:46:29 -0500

    Saving this

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/RRTeleOp.java
@@ -0,0 +1,180 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.google.gson.internal.$Gson$Preconditions;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.threadedhardware.HardwareThread;
+import org.firstinspires.ftc.teamcode.threadedhardware.RoadRunnerConfiguration;
+import org.firstinspires.ftc.teamcode.threadedhardware.SampleConfiguration;
+import org.firstinspires.ftc.teamcode.threadedhardware.Sequence;
+
+@Config
+@TeleOp(name="ScrimTeleOp")
+public class RRTeleOp extends LinearOpMode {
+
+    RoadRunnerConfiguration config;
+
+    boolean turning = false;
+
+    public static double OPEN = 0.02, CLOSE = 0.69, FLIPDOWN = 1, x = 0, y = -10; //0.23 dropper position to for auto lowest level
+
+    public static int[] levels = {0, 660, 1800, 2500};
+
+    private int currentLevel = 0;
+
+    public boolean levelPressed = false;
+
+    public double slidesOffset = 0;
+
+    Thread waitThread, dumpThread, testThread;
+
+    HardwareThread hardware;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        config = new RoadRunnerConfiguration(hardwareMap);
+        hardware = new HardwareThread(hardwareMap, config);
+        hardware.start();
+
+        double lastHeading = 0, ingesterSpeed = 0;
+
+        config.imu.gettingInput = true;
+
+        sleep(1000);
+
+        config.slides.setPower(-0.5);
+        while(!isStopRequested() && config.limit.get()[0] == 0) {}
+        config.slides.setPower(0);
+
+        slidesOffset = config.slides.get()[1];
+
+        waitForStart();
+
+        Sequence waitMillis = new Sequence(() -> {
+            sleep(500);
+            turning = false;
+        }, null);
+        waitThread = new Thread(waitMillis);
+
+        Sequence dump = new Sequence(() -> {
+            config.dropper.set(OPEN);
+            sleep(650);
+            config.dropper.set(CLOSE);
+            currentLevel = 0;
+        });
+        dumpThread = new Thread(dump);
+
+        Sequence forward = new Sequence(() -> {
+            Trajectory traj = config.trajectoryBuilder(config.getPoseEstimate())
+                    .forward(20)
+                    .build();
+            config.followTrajectory(traj);
+
+            Trajectory traj2 = config.trajectoryBuilder(new Pose2d(0, 0))
+                    .strafeTo(new Vector2d(x, y))
+                    .build();
+            config.followTrajectory(traj2);
+        });
+        testThread = new Thread(forward);
+
+        while(!isStopRequested()) {
+
+            hardware.waitForCycle();
+
+            config.ingest.setPower(ingesterSpeed);
+            config.spinner.setPower(gamepad2.left_stick_y);
+            config.preIngest.setPower(ingesterSpeed * 0.6);
+
+            if(gamepad1.back) config.flipdown.set(FLIPDOWN);
+
+            if(gamepad1.a || gamepad2.a) ingesterSpeed = 1;
+            else if(gamepad1.y || gamepad2.y) ingesterSpeed = -1;
+            else if(gamepad1.back || gamepad2.back) ingesterSpeed = 0;
+
+            double imuHeading = config.imu.get()[0];
+            double tempHeading = imuHeading;
+            double tempTarget = lastHeading;
+            if(tempHeading < 0) tempHeading += 2 * Math.PI;
+            if(lastHeading < 0) tempTarget += 2 * Math.PI;
+            double invert = lastHeading - imuHeading;
+            if(invert > Math.PI) invert -= 2 * Math.PI;
+            else if(invert < -Math.PI) invert += 2 * Math.PI;
+            invert = invert < 0 ? 1 : -1;
+            double power = invert * 0.8 * (Math.abs(tempHeading - tempTarget) > Math.PI ? (Math.abs(tempHeading > Math.PI ? 2 * Math.PI - tempHeading : tempHeading) + Math.abs(tempTarget > Math.PI ? 2 * Math.PI - tempTarget : tempTarget)) : Math.abs(tempHeading - tempTarget)); //Long line, but the gist is if you're calculating speed in the wrong direction, git gud.
+            if(Math.abs(power) < 0.05) power *= 0.5;
+            if(Math.abs(gamepad1.right_stick_x) > 0.1) turning = true;
+
+            else if(turning && !waitThread.isAlive()) waitThread.start();
+
+            //config.slides.setPower(gamepad2.left_stick_y);
+            if((gamepad1.dpad_up || gamepad2.dpad_up) && !levelPressed) {
+                currentLevel += currentLevel < 3 ? 1 : 0;
+                levelPressed = true;
+            }
+            else if((gamepad1.dpad_down || gamepad2.dpad_down) && !levelPressed) {
+                currentLevel -= currentLevel > 0 ? 1 : 0;
+                levelPressed = true;
+            }
+            else if(!gamepad1.dpad_down && !gamepad1.dpad_up && !gamepad2.dpad_down && !gamepad2.dpad_up) levelPressed = false;
+
+            double tempPos = config.slides.get()[1] - slidesOffset;
+
+            double pow = tempPos > levels[currentLevel] ? -1 : 1;
+
+            if(Math.abs(tempPos - levels[currentLevel]) < 150 || (pow == -1 && config.limit.get()[0] == 1)) pow = 0;
+            //if(Math.abs(tempPos - levels[currentLevel]) < 150) pow = 0.001;
+
+            config.slides.setPower((!gamepad2.left_bumper ? pow : (config.limit.get()[0] != 1 || gamepad2.right_stick_y > 0) ? gamepad2.right_stick_y : 0));
+            //double pow1 = Math.abs(gamepad2.right_stick_y) < 0.3 ? 0 : gamepad2.right_stick_y;
+            //if((tempPos <= 0 && pow1 < 0) || config.limit.get()[0] == 1) pow1 = 0;
+
+            //config.slides.setPower(pow);
+
+            if(turning || gamepad1.left_trigger > 0.3 || gamepad1.right_trigger > 0.3) {
+                lastHeading = imuHeading;
+                power = 0;
+            }
+
+            if(dumpThread.isAlive());
+            else if(gamepad1.start) dumpThread.start();
+            else if(gamepad1.x) config.dropper.set(CLOSE);
+            else if(gamepad1.b) config.dropper.set(OPEN);
+
+            double speed = gamepad1.right_bumper ? 0.3 : 1;
+            double x = 0.6 * -gamepad1.left_trigger + 0.6 * gamepad1.right_trigger + gamepad1.left_stick_x, y = gamepad1.left_stick_y, a = gamepad1.right_stick_x;
+
+            if(gamepad1.dpad_left && !testThread.isAlive()) testThread.start();
+
+            if(!testThread.isAlive()) setPower(speed * x, -speed * y, speed * a + power);
+
+            if(gamepad1.dpad_left) {
+
+            }
+
+            telemetry.addData("Heading: ", imuHeading);
+            telemetry.addData("Power: ", power);
+            telemetry.addData("Last Heading: ", lastHeading);
+            telemetry.addData("Level: ", currentLevel);
+            telemetry.addData("Slide Height: ", tempPos);
+            //telemetry.addData("Drivetrain Current Draw: ");
+            //telemetry.addData("Limit: ", config.limit.get()[0]);
+           // telemetry.addData("Expected Height: ", levels[currentLevel]);
+            telemetry.update();
+        }
+
+        hardware.Stop();
+    }
+
+    public void setPower(double x, double y, double a){
+        config.backLeft.setPower(-x + y + a);
+        config.frontLeft.setPower(x + y + a);
+        config.frontRight.setPower(-x + y - a);
+        config.backRight.setPower(x + y - a);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/threadedhardware/RoadRunnerConfiguration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/threadedhardware/RoadRunnerConfiguration.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.threadedhardware;/*package threadedhardwa
 /*
  * Simple mecanum drive hardware implementation for REV hardware.
  */
-/*
+
 import androidx.annotation.NonNull;
 
 //Like all the other RoadRunner stuff, CHANGE THESE IMPORT STATEMENTS to match your programs.
@@ -23,36 +23,42 @@ import com.acmerobotics.roadrunner.profile.MotionProfileGenerator;
 import com.acmerobotics.roadrunner.profile.MotionState;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
-import com.acmerobotics.roadrunner.trajectory.constraints.DriveConstraints;
-import com.acmerobotics.roadrunner.trajectory.constraints.MecanumConstraints;
+import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryAccelerationConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
 import com.acmerobotics.roadrunner.util.NanoClock;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 
-import org.firstinspires.ftc.teamcode.Autonomous.RoadRunner.DriveConstants;
 
-import threadedhardware.ThreadedHardware;
-import threadedhardware.ThreadedTrackingWheelLocalizer;
-import org.firstinspires.ftc.teamcode.Utils.DashboardUtil;
-import org.firstinspires.ftc.teamcode.Utils.LynxModuleUtil;
+import org.firstinspires.ftc.teamcode.Autonomous.NewRoadRunner.DriveConstants;
+import com.acmerobotics.roadrunner.trajectory.constraints.MecanumVelocityConstraint;
+
+import org.firstinspires.ftc.teamcode.Autonomous.NewRoadRunner.trajectorysequence.TrajectorySequenceBuilder;
+import org.firstinspires.ftc.teamcode.Autonomous.NewRoadRunner.trajectorysequence.TrajectorySequenceRunner;
+import org.firstinspires.ftc.teamcode.util.AxesSigns;
+import org.firstinspires.ftc.teamcode.util.BNO055IMUUtil;
+import org.firstinspires.ftc.teamcode.util.LynxModuleUtil;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import threadedhardware.Configuration;
-import threadedhardware.ThreadedIMU;
-import threadedhardware.ThreadedMotor;
-
-import static org.firstinspires.ftc.teamcode.RoadRunner.DriveConstants.BASE_CONSTRAINTS;
-import static org.firstinspires.ftc.teamcode.RoadRunner.DriveConstants.MOTOR_VELO_PID;
-import static org.firstinspires.ftc.teamcode.RoadRunner.DriveConstants.RUN_USING_ENCODER;
-import static org.firstinspires.ftc.teamcode.RoadRunner.DriveConstants.TRACK_WIDTH;
-import static org.firstinspires.ftc.teamcode.RoadRunner.DriveConstants.encoderTicksToInches;
+import static org.firstinspires.ftc.teamcode.Autonomous.NewRoadRunner.DriveConstants.MAX_ACCEL;
+import static org.firstinspires.ftc.teamcode.Autonomous.NewRoadRunner.DriveConstants.MAX_ANG_ACCEL;
+import static org.firstinspires.ftc.teamcode.Autonomous.NewRoadRunner.DriveConstants.MAX_ANG_VEL;
+import static org.firstinspires.ftc.teamcode.Autonomous.NewRoadRunner.DriveConstants.MAX_VEL;
+import static org.firstinspires.ftc.teamcode.Autonomous.NewRoadRunner.DriveConstants.MOTOR_VELO_PID;
+import static org.firstinspires.ftc.teamcode.Autonomous.NewRoadRunner.DriveConstants.RUN_USING_ENCODER;
+import static org.firstinspires.ftc.teamcode.Autonomous.NewRoadRunner.DriveConstants.TRACK_WIDTH;
+import static org.firstinspires.ftc.teamcode.Autonomous.NewRoadRunner.DriveConstants.encoderTicksToInches;
+import static org.firstinspires.ftc.teamcode.Autonomous.NewRoadRunner.SampleMecanumDrive.axes;
+import static org.firstinspires.ftc.teamcode.Autonomous.NewRoadRunner.SampleMecanumDrive.getAccelerationConstraint;
+import static org.firstinspires.ftc.teamcode.Autonomous.NewRoadRunner.SampleMecanumDrive.getVelocityConstraint;
 
 @Config
 public class RoadRunnerConfiguration extends MecanumDrive implements Configuration {
@@ -62,6 +68,8 @@ public class RoadRunnerConfiguration extends MecanumDrive implements Configurati
 
     public static double LATERAL_MULTIPLIER = 1.04;
     public static double inchMult = 86, offset = 0.135;
+
+    private TrajectorySequenceRunner trajectorySequenceRunner;
 
     public enum Mode {
         IDLE,
@@ -78,18 +86,21 @@ public class RoadRunnerConfiguration extends MecanumDrive implements Configurati
     private MotionProfile turnProfile;
     private double turnStart;
 
-    private DriveConstraints constraints;
     private TrajectoryFollower follower;
 
-    private List<Pose2d> poseHistory;
-
-    private Pose2d lastPoseOnTurn;
-
-    public ThreadedMotor leftFront, leftRear, rightRear, rightFront;
-    public List<ThreadedMotor> motors;
     private List<LynxModule> allHubs;
-    public ThreadedIMU imu;
-    private VoltageSensor batteryVoltageSensor;
+
+    public ThreadedMotor backLeft, frontLeft, frontRight, backRight, spinner, slides, ingest, preIngest;
+    private List<ThreadedMotor> motors;
+
+    public ThreadedServo dropper, flipdown;
+
+    public ThreadedDigitalSensor limit;
+
+    public ThreadedIMU imu; //A gyroscope in the expansion hubs (inertial measurement unit)
+
+    private static final TrajectoryVelocityConstraint VEL_CONSTRAINT = getVelocityConstraint(MAX_VEL, MAX_ANG_VEL, TRACK_WIDTH);
+    private static final TrajectoryAccelerationConstraint ACCEL_CONSTRAINT = getAccelerationConstraint(MAX_ACCEL);
 
     //Separated constructor and "Configure" method.
     public RoadRunnerConfiguration(HardwareMap hardwareMap) {
@@ -105,15 +116,10 @@ public class RoadRunnerConfiguration extends MecanumDrive implements Configurati
         turnController = new PIDFController(HEADING_PID);
         turnController.setInputBounds(0, 2 * Math.PI);
 
-        constraints = new MecanumConstraints(BASE_CONSTRAINTS, TRACK_WIDTH);
         follower = new HolonomicPIDVAFollower(TRANSLATIONAL_PID, TRANSLATIONAL_PID, HEADING_PID,
                 new Pose2d(0.5, 0.5, Math.toRadians(5.0)), 0.5);
 
-        poseHistory = new ArrayList<>();
-
         LynxModuleUtil.ensureMinimumFirmwareVersion(hardwareMap);
-
-        batteryVoltageSensor = hardwareMap.voltageSensor.iterator().next();
 
         allHubs = hardwareMap.getAll(LynxModule.class);
     }
@@ -122,16 +128,16 @@ public class RoadRunnerConfiguration extends MecanumDrive implements Configurati
 
         hardware.clear();
 
-        leftFront = new ThreadedMotor(hwMap, "front_left_motor");
-        leftRear = new ThreadedMotor(hwMap, "back_left_motor");
-        rightRear = new ThreadedMotor(hwMap, "back_right_motor");
-        rightFront = new ThreadedMotor(hwMap, "front_right_motor");
+        frontLeft = new ThreadedMotor(hwMap, "front_left_motor");
+        backLeft = new ThreadedMotor(hwMap, "back_left_motor");
+        backRight = new ThreadedMotor(hwMap, "back_right_motor");
+        frontRight = new ThreadedMotor(hwMap, "front_right_motor");
         imu = new ThreadedIMU(hwMap);
-        motors = Arrays.asList(leftFront, leftRear, rightRear, rightFront);
+        motors = Arrays.asList(frontLeft, backLeft, backRight, frontRight);
 
         // TODO: if your hub is mounted vertically, remap the IMU axes so that the z-axis points
         // upward (normal to the floor) using a command like the following:
-        // BNO055IMUUtil.remapAxes(imu, AxesOrder.XYZ, AxesSigns.NPN);
+        BNO055IMUUtil.remapAxes(imu, axes, AxesSigns.NPN);
 
         for (ThreadedMotor motor : motors) {
             MotorConfigurationType motorConfigurationType = motor.getMotorType().clone();
@@ -154,6 +160,8 @@ public class RoadRunnerConfiguration extends MecanumDrive implements Configurati
         motors.get(1).reverse(true);
 
         // TODO: if desired, use setLocalizer() to change the localization method.
+
+        trajectorySequenceRunner = new TrajectorySequenceRunner(follower, HEADING_PID);
     }
 
     public void setBulkCachingManual(boolean manual){
@@ -173,32 +181,31 @@ public class RoadRunnerConfiguration extends MecanumDrive implements Configurati
     }
 
     public TrajectoryBuilder trajectoryBuilder(Pose2d startPose) {
-        return new TrajectoryBuilder(startPose, constraints);
+        return new TrajectoryBuilder(startPose, VEL_CONSTRAINT, ACCEL_CONSTRAINT);
     }
 
     public TrajectoryBuilder trajectoryBuilder(Pose2d startPose, boolean reversed) {
-        return new TrajectoryBuilder(startPose, reversed, constraints);
+        return new TrajectoryBuilder(startPose, reversed, VEL_CONSTRAINT, ACCEL_CONSTRAINT);
     }
 
     public TrajectoryBuilder trajectoryBuilder(Pose2d startPose, double startHeading) {
-        return new TrajectoryBuilder(startPose, startHeading, constraints);
+        return new TrajectoryBuilder(startPose, startHeading, VEL_CONSTRAINT, ACCEL_CONSTRAINT);
+    }
+
+    public TrajectorySequenceBuilder trajectorySequenceBuilder(Pose2d startPose) {
+        return new TrajectorySequenceBuilder(
+                startPose,
+                VEL_CONSTRAINT, ACCEL_CONSTRAINT,
+                MAX_ANG_VEL, MAX_ANG_ACCEL
+        );
     }
 
     public void turnAsync(double angle) {
-        double heading = getPoseEstimate().getHeading();
-
-        lastPoseOnTurn = getPoseEstimate();
-
-        turnProfile = MotionProfileGenerator.generateSimpleMotionProfile(
-                new MotionState(heading, 0, 0, 0),
-                new MotionState(heading + angle, 0, 0, 0),
-                constraints.maxAngVel,
-                constraints.maxAngAccel,
-                constraints.maxAngJerk
+        trajectorySequenceRunner.followTrajectorySequenceAsync(
+                trajectorySequenceBuilder(getPoseEstimate())
+                        .turn(angle)
+                        .build()
         );
-
-        turnStart = clock.seconds();
-        mode = Mode.TURN;
     }
 
     public void turn(double angle) {
@@ -230,85 +237,8 @@ public class RoadRunnerConfiguration extends MecanumDrive implements Configurati
 
     public void update() {
         updatePoseEstimate();
-
-        Pose2d currentPose = getPoseEstimate();
-        Pose2d lastError = getLastError();
-
-        poseHistory.add(currentPose);
-
-        TelemetryPacket packet = new TelemetryPacket();
-        Canvas fieldOverlay = packet.fieldOverlay();
-
-        packet.put("mode", mode);
-
-        packet.put("x", currentPose.getX());
-        packet.put("y", currentPose.getY());
-        packet.put("heading", currentPose.getHeading());
-
-        packet.put("xError", lastError.getX());
-        packet.put("yError", lastError.getY());
-        packet.put("headingError", lastError.getHeading());
-
-        switch (mode) {
-            case IDLE:
-                // do nothing
-                break;
-            case TURN: {
-                double t = clock.seconds() - turnStart;
-
-                MotionState targetState = turnProfile.get(t);
-
-                turnController.setTargetPosition(targetState.getX());
-
-                double correction = turnController.update(currentPose.getHeading());
-
-                double targetOmega = targetState.getV();
-                double targetAlpha = targetState.getA();
-                setDriveSignal(new DriveSignal(new Pose2d(
-                        0, 0, targetOmega + correction
-                ), new Pose2d(
-                        0, 0, targetAlpha
-                )));
-
-                Pose2d newPose = lastPoseOnTurn.copy(lastPoseOnTurn.getX(), lastPoseOnTurn.getY(), targetState.getX());
-
-                fieldOverlay.setStroke("#4CAF50");
-                DashboardUtil.drawRobot(fieldOverlay, newPose);
-
-                if (t >= turnProfile.duration()) {
-                    mode = Mode.IDLE;
-                    setDriveSignal(new DriveSignal());
-                }
-
-                break;
-            }
-            case FOLLOW_TRAJECTORY: {
-                setDriveSignal(follower.update(currentPose));
-
-                Trajectory trajectory = follower.getTrajectory();
-
-                fieldOverlay.setStrokeWidth(1);
-                fieldOverlay.setStroke("#4CAF50");
-                DashboardUtil.drawSampledPath(fieldOverlay, trajectory.getPath());
-                double t = follower.elapsedTime();
-                DashboardUtil.drawRobot(fieldOverlay, trajectory.get(t));
-
-                fieldOverlay.setStroke("#3F51B5");
-                DashboardUtil.drawPoseHistory(fieldOverlay, poseHistory);
-
-                if (!follower.isFollowing()) {
-                    mode = Mode.IDLE;
-                    setDriveSignal(new DriveSignal());
-                }
-
-                break;
-            }
-        }
-
-        fieldOverlay.setStroke("#3F51B5");
-        DashboardUtil.drawRobot(fieldOverlay, currentPose);
-
-        dashboard.sendTelemetryPacket(packet);
+        DriveSignal signal = trajectorySequenceRunner.update(getPoseEstimate(), getPoseVelocity());
+        if (signal != null) setDriveSignal(signal);
     }
 
     public void waitForIdle() {
@@ -332,12 +262,6 @@ public class RoadRunnerConfiguration extends MecanumDrive implements Configurati
         for (ThreadedMotor motor : motors) {
             motor.setZeroPowerBehavior(zeroPowerBehavior);
         }
-    }
-
-    public PIDFCoefficients getPIDFCoefficients() { //Removed DcMotor.RunMode runMode
-        double[] temp = motors.get(0).getPID();
-        PIDFCoefficients coefficients = new PIDFCoefficients(temp[0], temp[1], temp[2], temp[3]); //Change last value later
-        return new PIDFCoefficients(coefficients.p, coefficients.i, coefficients.d, coefficients.f);
     }
 
     public void setPIDFCoefficients(PIDFCoefficients coefficients) { //Removed DcMotor.RunMode runMode
@@ -378,4 +302,3 @@ public class RoadRunnerConfiguration extends MecanumDrive implements Configurati
         return imu.get()[0];
     }
 }
- */
